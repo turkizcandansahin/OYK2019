32d69dde6a74a819bc534287e8e4eb72cee05343
Turkiz Candan Sahin <turkizcandansahin@gmail.com>
2019-07-26 11:34:14 +0300

    database'e 2 sayı eklendi

---
+++ sessionBireysel/index.php
@@ -0,0 +1,36 @@
+<?php
+include("db.php");
+
+if (isset( $_POST["sayi1"] )) {  // Form POST edilmiş...
+
+  // Önce EKLEME için SQL hazırlayalım...
+  $SQL = sprintf("
+      INSERT INTO
+        sayilar
+      SET
+        sayi1    = '%s',
+        sayi2 = '%s'",
+  $_POST["sayi1"], $_POST["sayi2"]);
+ echo $SQL;
+  // SQL komutunu MySQL veritabanı üzerinde çalıştır!
+  $rows  = mysqli_query($db, $SQL);
+
+  echo "<h4>Kayıt eklendi...</h4>";
+
+}  // Form POST edilmiş...
+
+?>
+
+
+    <form method="post">
+
+      1. Sayıyı Giriniz:
+      <input type="text" name="sayi1" value="" placeholder="Sayı giriniz">
+      <br><br />
+      2. Sayıyı Giriniz:
+      <input type="text" name="sayi2" value="" placeholder="Sayı giriniz">
+      <br><br>
+
+      <input type="submit" value="Gönder">
+
+    </form>
